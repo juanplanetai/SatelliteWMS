701bd59e7833752886291fad80be8e31597d9219
juanplanetai <jesteban.agudelo@planetai.space>
2024-06-14 06:31:23 -0500

    update on sentinel hub scripts

--- Copernicus/Sentinel-Hub/sentinel-hub.js
+++ Copernicus/Sentinel-Hub/sentinel-hub.js
@@ -1,23 +1,55 @@
+
+const lat = 2.771047, lon = -69.371827;
+
+function epsg4326toEpsg3857_lat(latitude) {
+    let y = 0;
+    y =
+      Math.log(Math.tan(((90 + latitude) * Math.PI) / 360)) /
+      (Math.PI / 180);
+    y = (y * 20037508.34) / 180;
+    return y;
+  }
+
+function epsg4326toEpsg3857_lon(longitude) {
+    let x = 0;
+    x = (longitude * 20037508.34) / 180;
+    return x;
+  }
+
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFmZWdvbnphbGV6IiwiYSI6ImNscnYyb3Z5MDBteGsya25hZWpmYTdsNzcifQ.3Rj3jRdf_iSTNOdT26iqUg';
     const map = new mapboxgl.Map({
         container: 'map',
         //maxZoom: 5.99,
         minZoom: 4,
-        zoom: 11,
-        center: [-75.789, 41.874],
+        zoom: 14,
+        center: [lon, lat],
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
         style: 'mapbox://styles/mapbox/dark-v11'
     });
 
+    
+    const coords = {north: lat+0.01,
+        south: lat-0.01,
+        west: lon-0.01,
+        east: lon+0.02
+    }
+
+    
+    console.log(epsg4326toEpsg3857_lon(coords.west))
+
     const INSTANCE_ID = "6cfcff16-7d83-45c8-a78f-7b81488fd4a1"
     const REQUEST = "GetMap"
-    const TIME = "2020-01-01T09:02:44Z/2020-02-01T11:00:00Z"
-    const BBOX = "-13152499,4038942,-13115771,4020692"
+    const TIME = "2023-01-15"
+    //const BBOX = `${coords.west},${coords.north},${coords.east},${coords.south}`
+    const BBOX = `${epsg4326toEpsg3857_lon(coords.west)}`+
+    `,${epsg4326toEpsg3857_lat(coords.north)}`+
+    `,${epsg4326toEpsg3857_lon(coords.east)}`+
+    `,${epsg4326toEpsg3857_lat(coords.south)}`
     const CRS = "EPSG:3857"
     const SRS = "EPSG:3857"
     const FORMAT = "image/png"
-    const RSX = "20m"
-    const RSY = "20m"
+    const RSX = "10m"
+    const RSY = "10m"
     const LAYERS = "NDWI"
     const EXCEPTIONS = "XML"
     const wms_image_url = `https://sh.dataspace.copernicus.eu/ogc/wms/${INSTANCE_ID}?REQUEST=${REQUEST}&`+
@@ -26,17 +58,15 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFmZWdvbnphbGV6IiwiYSI6ImNscnYyb3Z5MDBteGsya
             `RESX=${RSX}&RESY=${RSY}&`+
             `LAYERS=${LAYERS}&`+
             `EXCEPTIONS=${EXCEPTIONS}`
-    console.log("everything running")
-    console.log(wms_image_url)
     map.on('load', () => {
         map.addSource('radar', {
             'type': 'image',
             'url': wms_image_url,
             'coordinates': [
-                [-80.425, 46.437],
-                [-71.516, 46.437],
-                [-71.516, 37.936],
-                [-80.425, 37.936]
+                [coords.west, coords.north],
+                [coords.east, coords.north],
+                [coords.east, coords.south],
+                [coords.west, coords.south]
             ]
         });
         map.addLayer({
